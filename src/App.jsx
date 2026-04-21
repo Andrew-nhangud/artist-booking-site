ebe214c5524d14a2b34b4a214bd5b5ec68313f72
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-21 10:27:43 +0200

    added a new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./styles/global.css";
 import { Landing } from "./pages/Landing/Landing.jsx";
 import { Browser } from "./pages/Browser/Browser.jsx";
+import { NotFound } from "./pages/NotFound/NotFound.jsx";
 
 function App() {
   return (
     <div>
       <Landing />
       <Browser />
+      <NotFound />
     </div>
   );
 }
