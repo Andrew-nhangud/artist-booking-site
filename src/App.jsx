5bec42f2b983f0973dd7e1d587971f55e835afc6
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-21 09:59:35 +0200

    added react routher logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./styles/global.css";
+import { Landing } from "./pages/Landing/Landing.jsx";
+import { Browser } from "./pages/Browser/Browser.jsx";
 
 function App() {
-  return;
+  return (
+    <div>
+      <Landing />
+      <Browser />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import { Landing } from "./pages/Landing/Landing.jsx";
+import { Browser } from "./pages/Browser/Browser.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/", // Default home view inside layout
+    element: <Landing />,
+    errorElement: <h1>404: Page Not Found</h1>,
+  },
+  {
+    path: "/browser", // Page accessible at /about
+    element: <Browser />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
