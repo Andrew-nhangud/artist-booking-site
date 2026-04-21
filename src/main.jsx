1e04fbc5dcdc83900f457d74cf4c8f40c0299e18
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-21 10:22:47 +0200

    updated the error element

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import { Landing } from "./pages/Landing/Landing.jsx";
 import { Browser } from "./pages/Browser/Browser.jsx";
+import { NotFound } from "./pages/NotFound/NotFound.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/", // Default home view inside layout
     element: <Landing />,
-    errorElement: <h1>404: Page Not Found</h1>,
+    errorElement: <NotFound />,
   },
   {
-    path: "/browser", // Page accessible at /about
+    path: "/browser", // Page accessible at /browser
     element: <Browser />,
   },
 ]);
