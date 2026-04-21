41f835473b0c8c14c89fe7ec453881412474f086
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-21 13:25:21 +0200

    feat: added markup for the navbar section

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <Link className="landing-link" to="/">
+            RÉSERVE
+          </Link>
+        </li>
+        <li>
+          <Link className="browser-link" to="/browser">
+            browser
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
