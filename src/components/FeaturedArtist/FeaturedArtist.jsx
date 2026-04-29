7cb178db3408aabe96991f72645c87eb911dc22f
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-04-29 11:33:12 +0200

    removed inline styles and added overlay container

--- src/components/FeaturedArtist/FeaturedArtist.jsx
+++ src/components/FeaturedArtist/FeaturedArtist.jsx
@@ -3,35 +3,35 @@ import ArtistData from "../../data/ArtistData";
 
 export const FeaturedArtist = () => {
   // Destructuring the specific artist data
-  const { image, name, location, genre } = ArtistData[7];
+  const { image, name, location, genre } = ArtistData[4];
 
-  // Safety check: if ArtistData[7] doesn't exist, the app won't crash
+  // Safety check: if ArtistData[4] doesn't exist, the app won't crash
   if (!name) return null;
 
   return (
     <section className="featured">
-      <div //styles of image
+      <div
+        className="featured-image"
         style={{
           backgroundImage: `url(${image})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "60vh", // setting the height of the image
-          overflow: "hidden", // hidding the overflow of the image
         }}
       >
-        <div className="ft-box">featured</div>
+        {/* Overlay for the featured artist image */}
+        <div className="overlay">
+          <div className="ft-box">featured</div>
 
-        {/* container for the whole feature profile */}
-        <div className="ft-info">
-          <p className="ft-name">{name}</p>
+          {/* container for the whole feature profile */}
+          <div className="ft-info">
+            <p className="ft-name">{name}</p>
 
-          {/* container for the artist information */}
-          <div className="artist-info">
-            <p>{location}</p>
-            <p>{genre}</p>
+            {/* container for the artist information */}
+            <div className="artist-info">
+              <p>{location}</p>
+              <p>{genre}</p>
+            </div>
+
+            <button className="ft-profile-btn">view profile</button>
           </div>
-
-          <button className="ft-profile-btn">view profile</button>
         </div>
       </div>
     </section>
